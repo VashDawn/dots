c41b52bd38e106a033a63f381db5d5b00407c3ab
vash <vash.d@outlook.com>
2018-01-26 17:12:33 +0800

    Rebiult oh-my-zsh sections.

--- omz_ins.sh
+++ omz_ins.sh
@@ -6,17 +6,17 @@ then
 	if [ -e ~/.zshrc ]; then mv ~/.zshrc ~/.zshrc.old; fi  # rename old .zshrc
 
 	git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-	cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 fi
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 
 # Set /etc/.oh-my-zsh/
 if [ -e ~/.oh-my-zsh ]
 then
-	cp -rf ./oh-my-zsh/custom ~/.oh-my-zsh
+	cp -rf ./omz_custom/* ~/.oh-my-zsh/custom/
 fi
 
-if grep -qP '^plugins=\(.+\)' ~/.zshrc
-then
-	plugins='plugins=(git colored-man-pages vi-mode autojump history history-substring-search)'
-	sed -ri "s/^plugins=\(.+\)/${plugins}/g" ~/.zshrc
-fi
+# if grep -qP '^plugins=\(.+\)' ~/.zshrc
+# then
+# 	plugins='plugins=(git colored-man-pages vi-mode autojump history history-substring-search)'
+# 	sed -ri "s/^plugins=\(.+\)/${plugins}/g" ~/.zshrc
+# fi
