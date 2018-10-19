0a52c0b1593e65d2d10532ad2db63048d247c4e6
Vash <vasn.d@outlook.com>
2018-10-19 17:23:46 +0000

    Format fix.

--- ipython/keybindings.py
+++ ipython/keybindings.py
@@ -2,13 +2,8 @@ from IPython import get_ipython
 from prompt_toolkit.enums import DEFAULT_BUFFER
 from prompt_toolkit.filters import HasFocus, ViInsertMode
 from prompt_toolkit.key_binding.vi_state import InputMode
+
 ip = get_ipython()
-
-
-def switch_to_navigation_mode(event):
-    vi_state = event.cli.vi_state
-    vi_state.reset(InputMode.NAVIGATION)
-
 
 if getattr(ip, 'pt_app'):
     kbs = ip.pt_app.key_bindings
